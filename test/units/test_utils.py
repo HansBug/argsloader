b351fdd02c95e926261e30b945963c8ad13062e7
HansBug <hansbug@buaa.edu.cn>
2022-03-04 10:41:17 +0800

    test(hansbug): complete test for utils

---
+++ test/units/test_utils.py
@@ -0,0 +1,30 @@
+import pytest
+
+from argsloader.base import ParseError
+from argsloader.units import keep, check, is_type, to_type
+
+
+@pytest.mark.unittest
+class TestUnitsUtils:
+    def test_keep(self):
+        k = keep()
+        assert k(2) == 2
+        assert k(None) is None
+
+        l = [1, 2, 3]
+        assert k(l) is l
+
+    def test_keep_singleton(self):
+        assert keep() is keep()
+
+    def test_check(self):
+        ck = check(is_type((float, int)) >> to_type(int))
+        assert ck(1) == 1
+        assert ck(1.3) == 1.3
+
+        with pytest.raises(ParseError) as ei:
+            ck('sdjf')
+
+        err = ei.value
+        assert isinstance(err, ParseError)
+        assert isinstance(err, TypeError)

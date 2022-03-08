4e6d1382fca446dab815859464b39edc6e94d75f
HansBug <hansbug@buaa.edu.cn>
2022-03-08 15:46:21 +0800

    fix, doc(hansbug): fix a bug in mathop's name && add documentation for binary and unary operations in mathop

--- argsloader/units/mathop.py
+++ argsloader/units/mathop.py
@@ -1,3 +1,4 @@
+from textwrap import dedent
 from types import MethodType
 from typing import Mapping, Any
 
@@ -26,7 +27,7 @@ S_ = SingletonMark('math_op_self')
 
 def _create_unary_op(op, name_=None, funcname=None):
     short_name = (name_ or op.__name__).strip().strip('_')
-    funcname = short_name or funcname
+    funcname = funcname or short_name
 
     class _UnaryOpUnit(CalculateUnit):
         __names__ = ('v1',)
@@ -43,10 +44,44 @@ def _create_unary_op(op, name_=None, funcname=None):
 
     @fassign(
         __name__=funcname,
-        __module__=_nonsense.__module__
+        __module__=_nonsense.__module__,
     )
     def _op_func(v1=S_) -> '_UnaryOpUnit':
         return _UnaryOpUnit(keep() if v1 is S_ else v1)
+
+    _op_func.__doc__ = dedent(rf"""
+        Overview:s
+            Get the {name_} unary operation unit.
+                
+        :param v1: First value, default means the ``keep()`` will be used. 
+        :return: {name_.capitalize()} operation unit.
+            
+        Examples::
+            - Simple usage
+            
+            >>> from argsloader.units import {funcname}
+            >>> u = {funcname}()
+            >>> u(10)
+            {_op_func()(10)}
+            >>> u(0)
+            {_op_func()(0)}
+            
+            - Inner nested usage
+
+            >>> u = {funcname}(lambda x: x + 2)
+            >>> u(8)
+            {_op_func(lambda x: x + 2)(8)}
+            >>> u(-2)
+            {_op_func(lambda x: x + 2)(-2)}
+            
+            - Prefix usage
+            
+            >>> u = (lambda x: x + 2) >> {funcname}()
+            >>> u(8)
+            {((lambda x: x + 2) >> _op_func())(8)}
+            >>> u(-2)
+            {((lambda x: x + 2) >> _op_func())(-2)}
+    """)
 
     return _op_func
 
@@ -60,9 +95,9 @@ neg = _create_unary_op(lambda x: -x, 'neg')
 not_ = _create_unary_op(lambda x: not x, 'not', 'not_')
 
 
-def _create_binary_op(op, name_, funcname=None, reduce=False):
+def _create_binary_op(op, name_, sign, funcname=None, reduce=False, nodoc=False):
     short_name = name_.strip().strip('_')
-    funcname = short_name or funcname
+    funcname = funcname or short_name
 
     class _BinaryOpUnit(CalculateUnit):
         __names__ = ('v1', 'v2',)
@@ -91,48 +126,120 @@ def _create_binary_op(op, name_, funcname=None, reduce=False):
     def _op_func_by(self, v2) -> '_BinaryOpUnit':
         return self(keep(), v2)
 
-    @fassign(
-        __name__=funcname,
-        __module__=_nonsense.__module__,
-    )
-    def _op_func(v1, *vs) -> '_BinaryOpUnit':
-        if reduce:
+    if reduce:
+        @fassign(
+            __name__=funcname,
+            __module__=_nonsense.__module__,
+        )
+        def _op_func(v1, *vs) -> '_BinaryOpUnit':
             cur = v1
             for iv in vs:
                 cur = _BinaryOpUnit(cur, iv)
             return cur
-        else:
-            return _BinaryOpUnit(v1, *vs)
 
-    _op_func.from_ = MethodType(_op_func_from, _op_func)
-    _op_func.by = MethodType(_op_func_by, _op_func)
+        _op_func.from_ = MethodType(_op_func_from, _op_func)
+        _op_func.by = MethodType(_op_func_by, _op_func)
+        if not nodoc:
+            _op_func.__doc__ = dedent(f"""
+                Overview:
+                    Get the {name_} binary operation unit.
+    
+                :param v1: First unit.
+                :param vs: Other units, multiple units are supported.
+                :return: {name_.capitalize()} operation unit.
+    
+                Examples::
+                    - Simple usage
+    
+                    >>> from argsloader.units import {funcname}, keep
+                    >>> u = {funcname}(keep(), 3)
+                    >>> u(23)  # 23 {sign} 3
+                    {_op_func(keep(), 3)(23)}
+    
+                    - Multiple usage
+
+                    >>> u = {funcname}(keep(), 3, lambda x: x * 2)  # 23 {sign} 3 {sign} (23 * 2)
+                    {_op_func(keep(), 3, lambda x: x * 2)(23)}
+    
+                    - Suffix from usage
+    
+                    >>> u = keep() >> {funcname}.from_(3)
+                    >>> u(23)  # 3 {sign} 23
+                    {(keep() >> _op_func.from_(3))(23)}
+    
+                    - Suffix by usage
+    
+                    >>> u = keep() >> {funcname}.by(3)
+                    >>> u(23)  # 23 {sign} 3
+                    {(keep() >> _op_func.by(3))(23)}
+            """)
+
+    else:
+        @fassign(
+            __name__=funcname,
+            __module__=_nonsense.__module__,
+        )
+        def _op_func(v1, v2) -> '_BinaryOpUnit':
+            return _BinaryOpUnit(v1, v2)
+
+        _op_func.from_ = MethodType(_op_func_from, _op_func)
+        _op_func.by = MethodType(_op_func_by, _op_func)
+        if not nodoc:
+            _op_func.__doc__ = dedent(f"""
+                Overview:
+                    Get the {name_} binary operation unit.
+    
+                :param v1: First unit.
+                :param v2: Second unit.
+                :return: {name_.capitalize()} operation unit.
+                
+                Examples::
+                    - Simple usage
+                    
+                    >>> from argsloader.units import {funcname}, keep
+                    >>> u = {funcname}(keep(), 3)
+                    >>> u(23)  # 23 {sign} 3
+                    {_op_func(keep(), 3)(23)}
+                    
+                    - Suffix from usage
+                    
+                    >>> u = keep() >> {funcname}.from_(3)
+                    >>> u(23)  # 3 {sign} 23
+                    {(keep() >> _op_func.from_(3))(23)}
+                    
+                    - Suffix by usage
+                    
+                    >>> u = keep() >> {funcname}.by(3)
+                    >>> u(23)  # 23 {sign} 3
+                    {(keep() >> _op_func.by(3))(23)}
+            """)
 
     return _op_func
 
 
 # math binary operation
-add = _create_binary_op(lambda x, y: x + y, 'add', reduce=True)
+add = _create_binary_op(lambda x, y: x + y, 'add', '+', reduce=True)
 plus = add
-sub = _create_binary_op(lambda x, y: x - y, 'sub')
+sub = _create_binary_op(lambda x, y: x - y, 'sub', '-')
 minus = sub
-mul = _create_binary_op(lambda x, y: x * y, 'mul', reduce=True)
-matmul = _create_binary_op(lambda x, y: x @ y, 'matmul', reduce=True)
-truediv = _create_binary_op(lambda x, y: x / y, 'truediv')
-floordiv = _create_binary_op(lambda x, y: x // y, 'floordiv')
-mod = _create_binary_op(lambda x, y: x % y, 'mod')
-pow_ = _create_binary_op(lambda x, y: x ** y, 'pow', 'pow_')
-lshift = _create_binary_op(lambda x, y: x << y, 'lshift')
-rshift = _create_binary_op(lambda x, y: x >> y, 'rshift')
-and_ = _create_binary_op(lambda x, y: x and y, 'and', 'and_', reduce=True)
-or_ = _create_binary_op(lambda x, y: x or y, 'or', 'or_', reduce=True)
-band = _create_binary_op(lambda x, y: x & y, 'band', reduce=True)
-bor = _create_binary_op(lambda x, y: x | y, 'bor', reduce=True)
-bxor = _create_binary_op(lambda x, y: x ^ y, 'bxor', reduce=True)
+mul = _create_binary_op(lambda x, y: x * y, 'mul', '*', reduce=True)
+matmul = _create_binary_op(lambda x, y: x @ y, 'matmul', '@', reduce=True, nodoc=True)
+truediv = _create_binary_op(lambda x, y: x / y, 'truediv', '/')
+floordiv = _create_binary_op(lambda x, y: x // y, 'floordiv', '//')
+mod = _create_binary_op(lambda x, y: x % y, 'mod', '%')
+pow_ = _create_binary_op(lambda x, y: x ** y, 'pow', '**', 'pow_')
+lshift = _create_binary_op(lambda x, y: x << y, 'lshift', '<<')
+rshift = _create_binary_op(lambda x, y: x >> y, 'rshift', '>>')
+and_ = _create_binary_op(lambda x, y: x and y, 'and', 'and', 'and_', reduce=True)
+or_ = _create_binary_op(lambda x, y: x or y, 'or', 'or', 'or_', reduce=True)
+band = _create_binary_op(lambda x, y: x & y, 'band', '&', reduce=True)
+bor = _create_binary_op(lambda x, y: x | y, 'bor', '|', reduce=True)
+bxor = _create_binary_op(lambda x, y: x ^ y, 'bxor', '^', reduce=True)
 
 
 def _create_binary_check(op, name_, sign, opsign, preposition, funcname=None):
     short_name = name_.strip().strip('_')
-    funcname = short_name or funcname
+    funcname = funcname or short_name
 
     class _BinaryCheckUnit(CalculateUnit):
         __names__ = ('v1', 'v2',)
---
+++ docs/source/api_doc/units/mathop.rst
@@ -0,0 +1,184 @@
+argsloader.units.mathop
+=================================
+
+.. currentmodule:: argsloader.units.mathop
+
+.. automodule:: argsloader.units.mathop
+
+
+abs\_
+--------------------------------
+
+.. autofunction:: abs_
+
+
+inv
+--------------------------------
+
+.. autofunction:: inv
+
+
+invert
+--------------------------------
+
+.. autofunction:: invert
+
+
+pos
+--------------------------------
+
+.. autofunction:: pos
+
+
+neg
+--------------------------------
+
+.. autofunction:: neg
+
+
+not\_
+--------------------------------
+
+.. autofunction:: not_
+
+
+add
+--------------------------------
+
+.. autofunction:: add
+
+
+plus
+--------------------------------
+
+.. autofunction:: plus
+
+
+sub
+--------------------------------
+
+.. autofunction:: sub
+
+
+minus
+--------------------------------
+
+.. autofunction:: minus
+
+
+mul
+--------------------------------
+
+.. autofunction:: mul
+
+
+matmul
+--------------------------------
+
+.. autofunction:: matmul
+
+    Similar to :func:`mul`, it can be used when matrices are multiplied together.
+
+
+truediv
+--------------------------------
+
+.. autofunction:: truediv
+
+
+floordiv
+--------------------------------
+
+.. autofunction:: floordiv
+
+
+mod
+--------------------------------
+
+.. autofunction:: mod
+
+
+pow\_
+--------------------------------
+
+.. autofunction:: pow_
+
+
+lshift
+--------------------------------
+
+.. autofunction:: lshift
+
+
+rshift
+--------------------------------
+
+.. autofunction:: rshift
+
+
+and\_
+--------------------------------
+
+.. autofunction:: and_
+
+
+or\_
+--------------------------------
+
+.. autofunction:: or_
+
+
+band
+--------------------------------
+
+.. autofunction:: band
+
+
+bor
+--------------------------------
+
+.. autofunction:: bor
+
+
+bxor
+--------------------------------
+
+.. autofunction:: bxor
+
+
+eq
+--------------------------------
+
+.. autofunction:: eq
+
+
+ne
+--------------------------------
+
+.. autofunction:: ne
+
+
+ge
+--------------------------------
+
+.. autofunction:: ge
+
+
+gt
+--------------------------------
+
+.. autofunction:: gt
+
+
+le
+--------------------------------
+
+.. autofunction:: le
+
+
+lt
+--------------------------------
+
+.. autofunction:: lt
+
+
